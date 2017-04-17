0c282aa15b4144b0fcfa1131d054c868150ae143
Jared De La Cruz <jared_de_la_cruz@yahoo.com>
2017-04-16 17:33:47 -0700

    change jenkins image to jareddlc/jenkins-with-docker-socket

--- docker-build.sh
+++ docker-build.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-docker kill load_balancer &> /dev/null
-KILL=$?
-if [ $KILL -ne 0 ]; then
-  echo "Docker kill failed"
-  #exit $KILL
-fi
-
-docker rm load_balancer &> /dev/null
-RM=$?
-if [ $RM -ne 0 ]; then
-  echo "Docker remove failed"
-  #exit $RM
-fi
-
 docker build -t loadbalancer .
 
 echo "Container: docker run -d --name load_balancer -p 80:80 -p 443:443 loadbalancer"
